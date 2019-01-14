21d0084a49f846e373e403293a37f55bb4852c87
Mokiros <Mokiros@yandex.ru>
2019-01-14 12:24:20 +0300

    An Update
    1. Added disconnect support
    2. Renamed and moved stuff around

--- main.lua
+++ main.lua
@@ -3,23 +3,34 @@ if game:GetService("RunService"):IsClient() then error("Script must be server-si
 local Player,game,owner = owner,game
 local RealPlayer = Player
 do
-	print("FE Compatibility code by Mokiros")
-	local rp = RealPlayer
-	script.Parent = rp.Character
-	
-	--RemoteEvent for communicating
-	local Event = Instance.new("RemoteEvent")
-	Event.Name = "UserInput_Event"
+	print("FE Compatibility code V2 by Mokiros")
+	local RealPlayer = RealPlayer
+	script.Parent = RealPlayer.Character
 
 	--Fake event to make stuff like Mouse.KeyDown work
+	local Disconnect_Function = function(this)
+		this[1].Functions[this[2]] = nil
+	end
+	local Disconnect_Metatable = {__index={disconnect=Disconnect_Function,Disconnect=Disconnect_Function}}
+	local FakeEvent_Metatable = {__index={
+		Connect = function(this,f)
+			local i = tostring(math.random(0,10000))
+			while this.Functions[i] do
+				i = tostring(math.random(0,10000))
+			end
+			this.Functions[i] = f
+			return setmetatable({this,i},Disconnect_Metatable)
+		end
+	}}
+	FakeEvent_Metatable.__index.connect = FakeEvent_Metatable.__index.Connect
 	local function fakeEvent()
-		local t = {_fakeEvent=true,Functions={},Connect=function(self,f)table.insert(self.Functions,f) end}
-		t.connect = t.Connect
-		return t
+		return setmetatable({Functions={}},FakeEvent_Metatable)
 	end
 
 	--Creating fake input objects with fake variables
-    local m = {Target=nil,Hit=CFrame.new(),KeyUp=fakeEvent(),KeyDown=fakeEvent(),Button1Up=fakeEvent(),Button1Down=fakeEvent(),Button2Up=fakeEvent(),Button2Down=fakeEvent()}
+    local FakeMouse = {Hit=CFrame.new(),KeyUp=fakeEvent(),KeyDown=fakeEvent(),Button1Up=fakeEvent(),Button1Down=fakeEvent(),Button2Up=fakeEvent(),Button2Down=fakeEvent()}
+    FakeMouse.keyUp = FakeMouse.KeyUp
+    FakeMouse.keyDown = FakeMouse.KeyDown
 	local UIS = {InputBegan=fakeEvent(),InputEnded=fakeEvent()}
 	local CAS = {Actions={},BindAction=function(self,name,fun,touch,...)
 		CAS.Actions[name] = fun and {Name=name,Function=fun,Keys={...}} or nil
@@ -28,28 +39,28 @@ do
 	CAS.UnbindAction = CAS.BindAction
 
 	--This function will trigger the events that have been :Connect()'ed
-	local function te(self,ev,...)
-		local t = m[ev]
-		if t and t._fakeEvent then
-			for _,f in pairs(t.Functions) do
-				f(...)
-			end
+	local function TriggerEvent(self,ev,...)
+		for _,f in pairs(self[ev].Functions) do
+			f(...)
 		end
 	end
-	m.TrigEvent = te
-	UIS.TrigEvent = te
+	FakeMouse.TriggerEvent = TriggerEvent
+	UIS.TriggerEvent = TriggerEvent
 
+	--Client communication
+	local Event = Instance.new("RemoteEvent")
+	Event.Name = "UserInput_Event"
 	Event.OnServerEvent:Connect(function(plr,io)
-	    if plr~=rp then return end
-		m.Target = io.Target
-		m.Hit = io.Hit
+	    if plr~=RealPlayer then return end
+		FakeMouse.Target = io.Target
+		FakeMouse.Hit = io.Hit
 		if not io.isMouse then
 			local b = io.UserInputState == Enum.UserInputState.Begin
 			if io.UserInputType == Enum.UserInputType.MouseButton1 then
-				return m:TrigEvent(b and "Button1Down" or "Button1Up")
+				return FakeMouse:TriggerEvent(b and "Button1Down" or "Button1Up")
 			end
 			if io.UserInputType == Enum.UserInputType.MouseButton2 then
-				return m:TrigEvent(b and "Button2Down" or "Button2Up")
+				return FakeMouse:TriggerEvent(b and "Button2Down" or "Button2Up")
 			end
 			for _,t in pairs(CAS.Actions) do
 				for _,k in pairs(t.Keys) do
@@ -58,18 +69,15 @@ do
 					end
 				end
 			end
-			m:TrigEvent(b and "KeyDown" or "KeyUp",io.KeyCode.Name:lower())
-			UIS:TrigEvent(b and "InputBegan" or "InputEnded",io,false)
+			FakeMouse:TriggerEvent(b and "KeyDown" or "KeyUp",io.KeyCode.Name:lower())
+			UIS:TriggerEvent(b and "InputBegan" or "InputEnded",io,false)
 	    end
 	end)
-	Event.Parent = NLS([==[
-	local Player = game:GetService("Players").LocalPlayer
-	local Event = script:WaitForChild("UserInput_Event")
-
-	local Mouse = Player:GetMouse()
+	Event.Parent = NLS([==[local Event = script:WaitForChild("UserInput_Event")
+	local Mouse = owner:GetMouse()
 	local UIS = game:GetService("UserInputService")
-	local input = function(io,a)
-		if a then return end
+	local input = function(io,RobloxHandled)
+		if RobloxHandled then return end
 		--Since InputObject is a client-side instance, we create and pass table instead
 		Event:FireServer({KeyCode=io.KeyCode,UserInputType=io.UserInputType,UserInputState=io.UserInputState,Hit=Mouse.Hit,Target=Mouse.Target})
 	end
@@ -77,21 +85,26 @@ do
 	UIS.InputEnded:Connect(input)
 
 	local h,t
-	--Give the server mouse data 30 times every second, but only if the values changed
+	--Give the server mouse data every second frame, but only if the values changed
 	--If player is not moving their mouse, client won't fire events
-	while wait(1/30) do
+	local HB = game:GetService("RunService").Heartbeat
+	while true do
 		if h~=Mouse.Hit or t~=Mouse.Target then
 			h,t=Mouse.Hit,Mouse.Target
 			Event:FireServer({isMouse=true,Target=t,Hit=h})
 		end
-	end]==],Player.Character)
+		--Wait 2 frames
+		for i=1,2 do
+			HB:Wait()
+		end
+	end]==],script)
 
 	----Sandboxed game object that allows the usage of client-side methods and services
 	--Real game object
-	local _rg = game
+	local RealGame = game
 
 	--Metatable for fake service
-	local fsmt = {
+	local FakeService_Metatable = {
 		__index = function(self,k)
 			local s = rawget(self,"_RealService")
 			if s then
@@ -105,23 +118,23 @@ do
 		end
 	}
 	local function FakeService(t,RealService)
-		t._RealService = typeof(RealService)=="string" and _rg:GetService(RealService) or RealService
-		return setmetatable(t,fsmt)
+		t._RealService = typeof(RealService)=="string" and RealGame:GetService(RealService) or RealService
+		return setmetatable(t,FakeService_Metatable)
 	end
 
 	--Fake game object
-	local g = {
+	local FakeGame = {
 		GetService = function(self,s)
-			return rawget(self,s) or _rg:GetService(s)
+			return rawget(self,s) or RealGame:GetService(s)
 		end,
 		Players = FakeService({
-			LocalPlayer = FakeService({GetMouse=function(self)return m end},Player)
+			LocalPlayer = FakeService({GetMouse=function(self)return FakeMouse end},Player)
 		},"Players"),
 		UserInputService = FakeService(UIS,"UserInputService"),
 		ContextActionService = FakeService(CAS,"ContextActionService"),
 		RunService = FakeService({
 			_btrs = {},
-			RenderStepped = _rg:GetService("RunService").Heartbeat,
+			RenderStepped = RealGame:GetService("RunService").Heartbeat,
 			BindToRenderStep = function(self,name,_,fun)
 				self._btrs[name] = self.Heartbeat:Connect(fun)
 			end,
@@ -130,9 +143,9 @@ do
 			end,
 		},"RunService")
 	}
-	rawset(g.Players,"localPlayer",g.Players.LocalPlayer)
-	g.service = g.GetService
-	FakeService(g,game)
+	rawset(FakeGame.Players,"localPlayer",FakeGame.Players.LocalPlayer)
+	FakeGame.service = FakeGame.GetService
+	FakeService(FakeGame,game)
 	--Changing owner to fake player object to support owner:GetMouse()
-	game,owner = g,g.Players.LocalPlayer
+	game,owner = FakeGame,FakeGame.Players.LocalPlayer
 end
